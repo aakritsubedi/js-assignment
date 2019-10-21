dc97b191cb88cf5acf2960b4ba80c64ce6e4d98c
Aakrit Subedi <aakritsubedi9@gmail.com>
2019-10-21 18:39:39 +0545

    Slider

--- js/4imgSlider.js
+++ js/4imgSlider.js
@@ -2,26 +2,12 @@ var div = document.createElement('div');
 
 
 var FPS=60;
-var FRAME_RATE =1000;
-//var FRAME_LIMIT = FRAME_RATE/FPS; 
+var FRAME_LIMIT = 1000/FPS;
 
-function indexingPos(index,that){
-    console.log(index);
-    //console.log(that);
-    console.log(that.parentElement.parentElement.parentElement.parentElement.children[0]);
-
-    // that.currentIndex = index;
-    var chgVal = -(index)*683;
-    that.parentElement.parentElement.parentElement.parentElement.children[0].style.left= chgVal +'px';
-    // console.log(that.currentIndex);
-}
-
-;(function(){
+(function(){
 
     //slider
-    function Slider(parentEle,startIndex,fps){
-        this.fps = fps;
-        this.FRAME_LIMIT = FRAME_RATE/fps;
+    function Slider(parentEle,startIndex){
         this.parentEle = parentEle;
         //images
         this.images = null;
@@ -30,9 +16,7 @@ function indexingPos(index,that){
         this.sumOfWidthOfImg = null;
 
         //firstImage to display
-        this.startIndex = startIndex;
-
-        this.currentIndex = null;
+        this.currentIndex = startIndex;
 
         this.widthOfWrapper = null;
         this.carouselWrapper = null;
@@ -52,6 +36,7 @@ function indexingPos(index,that){
                 this.images[i].style.height=screenHt/2+'px';
             }
         }
+
         this.imgInfo = function(){
             //widthOfAllimg and Storing the sum of all width
             this.sumOfWidthOfImg = 0;
@@ -98,14 +83,11 @@ function indexingPos(index,that){
         
             
         }
-        //image change to function
+        
         var that = this;
-        function imgChgTo(index){
-            that.posX = index * that.widthOfImg[index];
-            that.imageWrapper.style.left=posX+'px'; 
 
-        }
         this.controller = function(){
+
             var div=document.createElement('div');
             var buttonLt=document.createElement('button');
             var buttonRt=document.createElement('button');
@@ -121,14 +103,23 @@ function indexingPos(index,that){
             ltBtn.setAttribute('id','ltArrow');
             ltBtn.innerHTML = '<';
 
+            buttonLt.onclick = function() {
+                that.setImage( (that.currentIndex == 0) ? (that.noOfImages - 1) : (that.currentIndex - 1) );                
+            };
+
             var rtBtn = controllerBox.appendChild(buttonRt);
             rtBtn.setAttribute('class','rtArrow');
             rtBtn.setAttribute('id','rtArrow');
             rtBtn.innerHTML = '>';
+
+            buttonRt.onclick = function() {
+                that.setImage((that.currentIndex + 1) % that.noOfImages);
+            };
             
             var indicator=controllerBox.appendChild(ul);
             indicator.setAttribute('class','indicator');
             for(var i=0; i<this.noOfImages; i++){
+
                 var btn = document.createElement('button');
                 btn.innerHTML = '0';
                 btn.style.fontSize = '25px';
@@ -136,8 +127,10 @@ function indexingPos(index,that){
                 btn.style.padding='2px 5px';
                 btn.style.background = 'none';
                 btn.style.color = '#7a0707';
-                btn.setAttribute('onclick', `indexingPos(${i},this.imageWrapper)`);
-                
+                btn.onclick = function(i) {
+                    return function() { that.setImage(i); };
+                }(i);
+
                 li.appendChild(btn);
                 li.style.listStyle = 'none';
                 li.style.padding = '7px 7px';
@@ -147,78 +140,12 @@ function indexingPos(index,that){
 
         }
 
-
-        this.displayFirstImg = function(){
-            this.currentIndex=this.startIndex;
-            //console.log(this.currentIndex);
-            this.imageWrapper.style.left = -this.currentIndex * this.widthOfImg[this.currentIndex]+'px';
-        }
-        
-        this.autoSlide= function(){
-            var that = this;
-            //console.log(that.currentIndex);
-            setInterval(function(){
-                if(that.currentIndex < 4){
-                    move = 1;
-                }
-                else if(that.currentIndex >= 4){
-                    move = -1;
-                }
-                else{
-                    var newVal = 0;
-                }
-            
-            that.imageWrapper.style.left = -(that.currentIndex + move) * that.widthOfImg[that.currentIndex]+'px';
-            that.currentIndex = that.currentIndex + move;
-            //console.log(that.currentIndex);
-            },100000/this.fps);
+        this.setImage = function(index) {
+            that.currentIndex = index;
+            var chgVal = -(index)*683;
+            that.imageWrapper.style.left= chgVal +'px';
         }
 
-        
-        
-        this.click = function(){
-            var controllerBox = this.parentEle.children[1];
-            var ltArrow = controllerBox.children[0];
-            var rtArrow = controllerBox.children[1];
-            var indexingBtn = controllerBox.children[2];
-            
-            var widthOfImg = this.widthOfImg[0];
-            //var index = this.currentIndex;
-            var that = this;
-            //console.log(widthOfImg);
-
-            var imgWrapperOfThis = controllerBox.parentElement.children[0];
-            //console.log(controllerBox);
-            console.log(ltArrow,"\n",rtArrow);
-            ltArrow.onclick = function(){
-                if(that.currentIndex != 4){
-                    var chgVal = (that.currentIndex+1)*widthOfImg;
-                    that.currentIndex = that.currentIndex +1;
-                    imgWrapperOfThis.style.left= -chgVal +'px';
-                    console.log(indexingBtn);
-                    console.log(that.currentIndex);
-                }
-                else{
-                    console.log("Cant Go");
-                }
-                //return chgVal;
-            }
-            rtArrow.onclick = function(){
-                if(that.currentIndex != 0){
-                    var chgVal = (that.currentIndex-1)*widthOfImg;
-                    that.currentIndex = that.currentIndex -1;
-                    imgWrapperOfThis.style.left= chgVal +'px';
-                    console.log(that.currentIndex);
-                }
-                else{
-                    console.log("Cant Go");
-                }
-                //return chgVal;
-            }
-            
-            //this.currentIndex + 1;
-            //console.log()
-        }
 
         
         this.init = function(){
@@ -230,13 +157,13 @@ function indexingPos(index,that){
             this.setCSS();
             
             this.controller();
-            this.displayFirstImg()
+        
         }
 
         this.init();
-        setInterval(this.autoSlide(),2000);
-        this.click();
-
     }
-    new Slider(document.getElementsByClassName('carousel-wrapper')[0],startIndex=0,fps=60);
+    new Slider(document.getElementsByClassName('carousel-wrapper')[0],startIndex=1);
+    new Slider(document.getElementsByClassName('carousel-wrapper')[1],startIndex=3);
+    new Slider(document.getElementsByClassName('carousel-wrapper')[2],startIndex=4);
+    new Slider(document.getElementsByClassName('carousel-wrapper')[3],startIndex=2);
 })();
